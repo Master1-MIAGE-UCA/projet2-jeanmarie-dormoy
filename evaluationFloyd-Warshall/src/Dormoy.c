5b9a898610c742b55276922654bff3d1d357564c
Jean-Marie Dormoy <jean-marie.dormoy@etu.univ-cotedazur.fr>
2020-05-23 16:22:18 +0200

    imported matrix product function easily paralellizable with openMP

--- evaluationFloyd-Warshall/src/Dormoy.c
+++ evaluationFloyd-Warshall/src/Dormoy.c
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <sys/time.h>
+#include <stdarg.h>
 #define LINE_SIZE 500
 
 typedef struct Matrix {
@@ -17,8 +18,11 @@ typedef struct Matrix {
 #define GET(mat, i, j) mat->data[i * mat->width + j]
 #define SET(mat, i, j, val) GET(mat, i, j) = val
 
-//TODO reverse paramters order and all calls
-Matrix *new_Matrix(int width, int height) {
+/* .---------------. 
+ * |  CONSTRUCTOR  |
+ * .---------------.
+ */
+Matrix *new_Matrix(int height, int width) {
 	Matrix *m = malloc(sizeof(Matrix));
 	if (!m) {
 		fprintf(stderr, "new_Matrix: Matrix malloc error\n");
@@ -35,7 +39,45 @@ Matrix *new_Matrix(int width, int height) {
 	}
 	return m;
 }
-
+void Destroy_Matrix(Matrix *m) {
+	if (m && m->data) {
+		free(m->data);
+		free(m);
+	}
+}
+void Destroy_All_Matrices(int num, ...) {
+	va_list valist; 
+	Matrix *temp; 
+    va_start(valist, num); 
+    for (int i = 0; i < num; i++) { 
+		temp = va_arg(valist, Matrix*);
+		Destroy_Matrix(temp);
+	}
+    va_end(valist); 
+}
+/* .-----------. 
+ * |  DISPLAY  |
+ * .-----------.
+ */
+void print_raw_matrix(Matrix *m) {
+	int len = m->width * m->height;
+	for (int i = 0; i < len; ++i)
+		printf("%d ", m->data[i]);
+	puts("");
+}
+void print_matrix(Matrix *m) {
+	int len = m->width * m->height;
+	for (int i = 0; i < len; ++i) {
+		if (i % m->width == 0)
+			if (i == 0)
+				printf("%5u ", m->data[i]);
+			else
+				printf("\n%5u ", m->data[i]);
+		else
+			printf("%5u ", m->data[i]);
+	}
+	puts("\n");
+}
 void randomlyFillMatrix(Matrix *m){
 	//Random seed
 	srandom(time(0)+clock()+random());
@@ -48,31 +90,34 @@ void randomlyFillMatrix(Matrix *m){
 Matrix *matrixcpy(Matrix *src) {
 	if (!src || !src->data)
 		return NULL;
-	Matrix *dest = new_Matrix(
-			src->width, src->height);
+	Matrix *dest = new_Matrix(src->height, src->width);
 	#pragma omp parallel for
-	for(int i=0; i < src->size; i++){
-			dest->data[i] = src->data[i];
-	}
+	for(int i=0; i < src->size; i++)
+		dest->data[i] = src->data[i];
 	return dest;
 }
 
-//TODO write function
 Matrix *matrixcpy_reverseIndex(Matrix *src) {
 	if (!src || !src->data)
 		return NULL;
-	Matrix *dest = new_Matrix(
-			src->width, src->height);
+	Matrix *res = new_Matrix(src->height, src->width);
 	#pragma omp parallel for
-	for(int i=0; i < src->height; i++){
-		for(int j=0; j < src->width; j++) {
-
-			dest->data[i] = src->data[i];
-	}
-	return dest;
+	for(int i=0; i < src->height; i++)
+		for(int j=0; j < src->width; j++)
+			res->data[j*src->height + i] = 
+				GET(src, i, j);
+	return res;
 }
 
-Matrix *sequentialMultiply(Matrix *a, Matrix *b, Matrix *res) {
+Matrix *sequentialMultiply(Matrix *a, Matrix *b) {
+	if (a->width != b->height) {
+		fprintf(
+				stderr, 
+				"matrix product: a->width=%d b->height=%d\n",
+				a->width, b->height);
+		exit(7);
+	}
+	Matrix *res = new_Matrix(a->height, b->width);
 	struct timeval t0, t1;
 	gettimeofday(&t0, 0);
 	for(int i=0; i < a->height; i++){
@@ -92,9 +137,11 @@ Matrix *sequentialMultiply(Matrix *a, Matrix *b, Matrix *res) {
 }
 
 Matrix *sequentialMultiplyBySelf(Matrix *m) {
-	Matrix *a, *b;
+	Matrix *a, *b, *res;
 	a = matrixcpy(m); b = matrixcpy(m);
-	return sequentialMultiply(a, b, m);
+	res = sequentialMultiply(a, b);
+	Destroy_All_Matrices(2, a, b);
+	return res;
 }
 
 
@@ -104,24 +151,31 @@ Matrix *parallelMultiply(Matrix *a, Matrix *b) {
 		fprintf(
 				stderr, 
 				"matrix product: a->width=%d b->height=%d\n",
-				a->width, b->width);
-		exit(7);
+				a->width, b->height);
+		exit(8);
 	}
 	Matrix *res = new_Matrix(a->height, b->width);
+	Matrix *convB = matrixcpy_reverseIndex(b);
 	int i, j, k, iOff, jOff, sum;
 	struct timeval t0, t1;
 	gettimeofday(&t0, 0);
 
-	#pragma omp parallel for private(i, j, k, iOff, jOff) \
-		shared(res, a, b, sum)
+	/*
+	puts("b:");
+	print_matrix(b);
+	puts("convB:");
+	print_matrix(convB);*/
+	
+	/*#pragma omp parallel for private(i, j, k, iOff, jOff) \
+	//	shared(res, a, b, sum)*/
 	for(i=0; i < a->height; i++){
 		iOff = i * a->width;
 		for(j=0; j < b->width; j++){
 			jOff = j * b->height;
 			sum = 0;
-			#pragma omp parallel for reduction(+: sum)
+			//#pragma omp parallel for reduction(+: sum)
 			for(k=0; k< a->width; k++){
-				sum += a->data[iOff + k] * b->data[jOff + k];
+				sum += a->data[iOff + k] * convB->data[jOff + k];
 			}
 			SET(res, i, j, sum);
 		}
@@ -132,12 +186,15 @@ Matrix *parallelMultiply(Matrix *a, Matrix *b) {
 		(t1.tv_usec - t0.tv_usec) / 1000000.0f;
 	printf("parallelMultiply time:  %f\n", elapsed);
 	return res;
+	//return NULL;
 }
 
 Matrix *parallelMultiplyBySelf(Matrix *m) {
-	Matrix *a, *b;
+	Matrix *a, *b, *res;
 	a = matrixcpy(m); b= matrixcpy(m);
-	return parallelMultiply(a, b);
+	res = parallelMultiply(a, b);
+	Destroy_All_Matrices(2, a, b);
+	return res;
 }
 
 int first_pass(char *s) {
@@ -151,23 +208,6 @@ int first_pass(char *s) {
 	return len;
 }
 
-void print_matrix(Matrix *m) {
-	int len = m->width * m->height;
-	for (int i = 0; i < len; ++i)
-		printf("%d ", m->data[i]);
-	puts("");
-}
-
-void pretty_matrix(Matrix *m) {
-	int len = m->width * m->height;
-	for (int i = 0; i < len; ++i) {
-		if (i % m->width == 0)
-			printf("\n%5u ", m->data[i]);
-		else
-			printf("%5u ", m->data[i]);
-	}
-	puts("");
-}
 
 void fill_matrix_with_line(
 		unsigned int *array, int *index, char *line) {
@@ -226,14 +266,15 @@ Matrix *build_matrix(
 	return res;
 }
 
+int Test_Equals(Matrix *a, Matrix *b);
 int main(int argc, char *argv[]) {
 
-    int rank, numprocs;// line_size = 0;
+    int rank, numprocs, equals;
 	FILE *fp;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	Matrix *mat, *res, *copy;
+	Matrix *mat, *a, *a_save, *b, *parallel_res, *seq_res;
 	//MPI_Status status;
 	if (argc != 2) {
 		fprintf(stderr,
@@ -250,26 +291,39 @@ int main(int argc, char *argv[]) {
 	switch(rank) {
 		case 0:
 			puts("process 0");
-			printf("argc= %d\n", argc);
-			printf("=>%s\n", argv[0]);
-			printf("=>%s\n", argv[1]);
+			//printf("argv[0]=%s\n", argv[0]);
 			mat = build_matrix(fp);
 			if (!mat) {
 				fprintf(stderr, "var mat: build_matrix ret NULL\n");
 				exit(6);
 			}
-			pretty_matrix(mat);
-			puts("-------------------");
-			res = new_Matrix(4, 4);
-			randomlyFillMatrix(res);
-			copy = matrixcpy(res);
-			puts("   res:");
-			pretty_matrix(res);			
+			//printf("mat written from %s:\n", argv[1]);
+			//print_matrix(mat);
+
+			a = new_Matrix(800, 414);
+			randomlyFillMatrix(a);
+			a_save = matrixcpy(a);
+			puts("a:");
+			print_matrix(a);			
+			
+			b = new_Matrix(414, 712);
+			randomlyFillMatrix(b);
+
+			puts("sequential a * b:");
+			seq_res = sequentialMultiply(a, b);
+			//print_matrix(seq_res);
 			puts("");
-			sequentialMultiplyBySelf(res);
-			pretty_matrix(res);
-			parallelMultiplyBySelf(copy);
-			pretty_matrix(copy);
+
+			puts("parallel a * b:");
+			parallel_res = parallelMultiply(a_save, b);
+			//print_matrix(parallel_res);
+
+			equals = Test_Equals(seq_res, parallel_res);
+			printf("seq_res == parallel_res: %s\n",
+					equals ? "yes" : "no");
+			Destroy_All_Matrices(
+					4,
+					mat, a, a_save, parallel_res);
 			break;
 		case 1:
 			puts("process 1");
@@ -278,3 +332,19 @@ int main(int argc, char *argv[]) {
 	MPI_Finalize();
 	return 0;
 }
+
+int Test_Equals(Matrix *a, Matrix *b) {
+	if (a->height != b->height 
+			|| a->width != b->width
+			|| a->size != b->size)
+		return 0;
+	for (int i = 0; i < a->size; i++)
+		if (a->data[i] != b->data[i]) {
+			printf("i=%d\n", i);
+			printf("a[i]=%d b[i]=%d\n",
+					a->data[i], b->data[i]);
+			return 0;
+		}
+	return 1;
+}
+
